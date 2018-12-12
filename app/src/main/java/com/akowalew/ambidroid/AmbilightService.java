c3549dcf706129e252213de5a3e7f62c757e3c87
akowalew <ram.techen@gmail.com>
2018-12-12 12:48:47 +0100

    Added AmbilightService, which at the moment just waits some time

---
+++ app/src/main/java/com/akowalew/ambidroid/AmbilightService.java
@@ -0,0 +1,25 @@
+package com.akowalew.ambidroid;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class AmbilightService extends IntentService {
+    private static final String TAG = "AmbilightService";
+
+    public AmbilightService() {
+        super("AmbilightService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            return;
+        }
+
+        Log.v(TAG, "Running...");
+        SystemClock.sleep(5000);
+        Log.v(TAG, "Finished");
+    }
+}
