d38b196685e28bf504f3caf2503e6cf603559b43
akowalew <ram.techen@gmail.com>
2018-12-13 14:01:16 +0100

    Included usb-serial-for-android library and added example script which connects to serial

--- app/src/main/java/com/akowalew/ambidroid/AmbilightService.java
+++ app/src/main/java/com/akowalew/ambidroid/AmbilightService.java
@@ -9,12 +9,22 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.io.IOException;
+import java.util.List;
 
 public class AmbilightService extends Service {
     public static final String START_ACTION = "com.akowalew.ambidroid.AmbilightService.START_ACTION";
@@ -80,11 +90,46 @@ public class AmbilightService extends Service {
             .setAutoCancel(false)
             .build();
 
+        serialExample();
+
         assert mNotificationId != 0;
         startForeground(mNotificationId, notification);
         if (mNotificationId != mLastNotificationId) {
             mNotificationManager.cancel(mLastNotificationId);
             mLastNotificationId = mNotificationId;
+        }
+    }
+
+    private void serialExample() {
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            Toast.makeText(this, "There are no available drivers", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            Toast.makeText(this, "Could not open connection", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        UsbSerialPort port = driver.getPorts().get(0);
+        try {
+            port.open(connection);
+            port.setParameters(921600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            byte buffer[] = new byte[144];
+            final int bytesWrite = port.write(buffer, 1000);
+            Toast.makeText(this, "Write " + bytesWrite + " bytes.", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "IOException error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                port.close();
+            } catch (IOException e) {
+                Toast.makeText(this, "IOException error during close: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         }
     }
 
